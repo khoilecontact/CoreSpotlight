7bcdbf6dfd4c8c1bfeb76891792f8626142f07e5
KhoiLe <Khoilecontact@gmail.com>
2021-08-06 18:26:04 +0700

    6/8 first commit

--- Swift Seacher _ 32/AppDelegate.swift
+++ Swift Seacher _ 32/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CoreSpotlight
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -31,6 +32,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
+    func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
+        if userActivity.activityType == CSSearchableItemActionType {
+            if let uniqueIdentifier = userActivity.userInfo?[CSSearchableItemActivityIdentifier] as? String {
+//                if let navigationController = window?.rootViewController as? UINavigationController {
+//                    if let viewController = navigationController.topViewController as? ViewController {
+//                        viewController.showTutorial(Int(uniqueIdentifier)!)
+//                    }
+//                }
+            }
+        }
 
+        return true
+    }
+    
 }
 
